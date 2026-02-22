3109dde12799c282dd25d95d06e123f85f5dda23
Thrapis <artbelski@gmail.com>
2026-02-22 21:23:18 +0300

    refactor: apply FSD methodology + add quality of life tweaks

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorldPage from '@/pages/HelloWorldPage.vue'
 </script>
 
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/favicon.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="../assets/images/svg/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorldPage msg="Vite + Vue" />
 </template>
 
 <style scoped>
