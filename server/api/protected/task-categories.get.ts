765e5ff86ee3c5a0aa75c2be2c3fed3e4b4ddd07
Thrapis <artbelski@gmail.com>
2026-03-24 20:06:01 +0300

    feat: add nuxt caching for immutable db data

--- server/api/protected/task-categories.get.ts
+++ server/api/protected/task-categories.get.ts
@@ -1,10 +1,21 @@
+import type { TaskCategory } from '~~/server/models/entities';
 import useRepositories from '~~/server/plugins/repositories';
+import { TASK_CATEGORIES_CACHE_KEY } from '~~/server/utils/constants';
 import { toArrayOfTaskCategoryDTO } from '~~/server/utils/converters/task-category.dto';
+import { setServerCache, useServerCache } from '~~/server/utils/storage';
 
 export default defineEventHandler(async (event) => {
+  let categories = await useServerCache<TaskCategory[]>(TASK_CATEGORIES_CACHE_KEY);
+
+  if (categories) {
+    return toArrayOfTaskCategoryDTO(categories);
+  }
+
   const { taskCategoryRepository } = await useRepositories(event);
 
-  const categories = await taskCategoryRepository.findAll();
+  categories = await taskCategoryRepository.findAll();
+
+  await setServerCache<TaskCategory[]>(TASK_CATEGORIES_CACHE_KEY, categories);
 
   return toArrayOfTaskCategoryDTO(categories);
 });
--- server/api/protected/task-difficulties.get.ts
+++ server/api/protected/task-difficulties.get.ts
@@ -1,10 +1,21 @@
+import type { TaskDifficulty } from '~~/server/models/entities';
 import useRepositories from '~~/server/plugins/repositories';
+import { TASK_DIFFICULTIES_CACHE_KEY } from '~~/server/utils/constants';
 import { toArrayOfTaskDifficultyDTO } from '~~/server/utils/converters/task-difficulty.converter';
+import { setServerCache, useServerCache } from '~~/server/utils/storage';
 
 export default defineEventHandler(async (event) => {
+  let difficulties = await useServerCache<TaskDifficulty[]>(TASK_DIFFICULTIES_CACHE_KEY);
+
+  if (difficulties) {
+    return toArrayOfTaskDifficultyDTO(difficulties);
+  }
+
   const { taskDifficultyRepository } = await useRepositories(event);
 
-  const difficulties = await taskDifficultyRepository.findAll();
+  difficulties = await taskDifficultyRepository.findAll();
+
+  await setServerCache<TaskDifficulty[]>(TASK_DIFFICULTIES_CACHE_KEY, difficulties);
 
   return toArrayOfTaskDifficultyDTO(difficulties);
 });
